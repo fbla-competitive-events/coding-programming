04dcdc7563b6c4f74a6d18532d26aed4a94f5fc6
Future Business Leaders of America-Phi Beta Lambda <50464888+FBLA-PBL@users.noreply.github.com>
2019-05-25 07:21:41 -0400

    2018 FBLA Coding & Programming, 10th place

---
+++ 2018/10th/bigsql/python2/Lib/site-packages/lmdb/_config.py
@@ -0,0 +1,2 @@
+CONFIG = dict((('extra_compile_args', ['-UNDEBUG', '-w', '/FIPython.h']), ('extra_sources', ['lib/mdb.c', 'lib/midl.c']), ('extra_library_dirs', []), ('extra_include_dirs', ['lib/py-lmdb', 'lib', 'lib\\win32-stdint', 'lib\\win32']), ('libraries', ['Advapi32'])))
+
