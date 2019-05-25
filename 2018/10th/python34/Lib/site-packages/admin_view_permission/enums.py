a1065ca6740cbd85e64f35112385a2957878dbdb
troydwhite <troydwhite@gmail.com>
2019-05-25 10:49:54 -0400

    2018 FBLA Coding & Programming, 10th place

---
+++ 2018/10th/python34/Lib/site-packages/admin_view_permission/enums.py
@@ -0,0 +1,5 @@
+from __future__ import unicode_literals
+
+
+class DjangoVersion(object):
+    DJANGO_18, DJANGO_19, DJANGO_110, DJANGO_111, DJANGO_20 = range(0, 5)
---
+++ 2018/10th/python34/Scripts/django-admin-script.py
@@ -0,0 +1,10 @@
+#!c:\ethanprice_ellensburg_wa_fbla_coding\python34\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'django==2.0','console_scripts','django-admin'
+__requires__ = 'django==2.0'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('django==2.0', 'console_scripts', 'django-admin')()
+    )
